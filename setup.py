7cd3486d01a17ec9db780e27bc74e88c89612615
Brian Alexander <balexander04@gmail.com>
2018-08-23 18:24:42 -0500

    Forgot to remove urls.py right now it doesn't do anything and bumped version 0.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-inventory',
-    version='0.2',
+    version='0.3',
     packages=find_packages(),
     install_requires=['pillow'],
     include_package_data=False,
